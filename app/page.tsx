f9d81ec3a0a6f18cfbbb4df55aaabf52107bfc02
nhuhuynh1508 <93415945+nhuhuynh1508@users.noreply.github.com>
2025-11-16 22:41:20 +0700

    Update packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import Box from "@/components/box";
 import Logo from "@/components/logo";
 
 export default function Home() {
     return (
-      <Logo/>
+      <div
+        className="w-full h-screen"
+        style={{
+          background: "linear-gradient(to top, white 50%, #cdeffcff 50%, #d6e0e7ff 100%)",
+        }}
+      >
+        <Logo/>
+        <div className="flex flex-col items-center justify-center mt-10 text-center gap-4">
+          <div className="text-2xl md:text-3xl font-semibold font-nunito">
+            Travel Smarter, Not Harder
+          </div>
+          <div className="text-base md:text-xl font-nunito max-w-xl md:max-w-3xl text-[#767689]">
+            Make every trip effortless. Tripzy lets you book rides and plan journeys for ease
+          </div>
+        </div>
+        <Box/>
+      </div>
     );
 }
