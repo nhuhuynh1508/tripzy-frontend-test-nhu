0e73a7a16bfe3ede335f3064be017af038685b99
nhuhuynh1508 <93415945+nhuhuynh1508@users.noreply.github.com>
2025-11-18 11:49:47 +0700

    Seperate Tab component into wrapper and smaller tabs

--- components/tab.tsx
+++ components/tab.tsx
@@ -1,19 +1,18 @@
 "use client"
+import { Bus, Hotel, Plane } from "lucide-react";
 
-import { Bus } from "lucide-react"
-import { Hotel } from "lucide-react"
-import { Plane } from "lucide-react"
-import { Blank } from "./blank";
-import { useState } from "react";
+type TabProps = {
+  activeTab: string;
+  setActiveTab: (tab: string) => void;
+};
 
-export default function Tab () {
-    const [activeTab, setActiveTab] = useState("bus");
 
+export default function Tab ({ activeTab, setActiveTab }: TabProps) {
     return (
-        <>
-        <div className="flex flex-col md:flex-row rounded-xl bg-white max-w-5xl mx-auto p-2 md:p-4 gap-2 md:gap-4 shadow-md">
+        <div className="flex flex-col md:flex-row rounded-xl bg-white p-2 md:p-4 gap-2 md:gap-4 shadow-md">
+            {/*Bus & Shuttle*/}
             <div 
-                className="flex rounded-md flex-1 md:px-5 px-3 py-2 gap-6 items-center justify-left hover:bg-[#EBF9FF]"
+                className="flex rounded-md flex-1 md:px-5 px-3 py-2 gap-6 items-center justify-left hover:bg-[#EBF9FF] cursor-pointer"
                 onClick={() => setActiveTab("bus")}
             >
                 <div className="w-10 h-10 md:w-12 md:h-12 rounded-full bg-[#D3F3FF] flex items-center justify-center">
@@ -22,18 +21,20 @@ export default function Tab () {
                 Bus & Shuttle
             </div>
 
+            {/*Hotel & Accommodation*/}
             <div 
-                className="flex rounded-md flex-1 md:px-5 px-3 py-2 gap-6 items-center justify-left hover:bg-[#F4FFEB]"
+                className="flex rounded-md flex-1 md:px-5 px-3 py-2 gap-6 items-center justify-left hover:bg-[#F4FFEB] cursor-pointer"
                 onClick={() => setActiveTab("hotel")}
             >
-                <div className="w-10 h-10 md:w-12 md:h-12 rounded-full bg-[#E8FBCC] flex items-center justify-center" >
+                <div className="w-10 h-10 md:w-12 md:h-12 rounded-full bg-[#E8FBCC] flex items-center justify-center">
                     <Hotel className="text-[#447A11]" />
                 </div>
                 Hotel & Accommodation
             </div>
 
+            {/*Flight*/}
             <div 
-                className="flex rounded-md flex-1 md:px-5 px-3 py-2 gap-6 items-center justify-left hover:bg-[#EBF4FF]" 
+                className="flex rounded-md flex-1 md:px-5 px-3 py-2 gap-6 items-center justify-left hover:bg-[#EBF4FF] cursor-pointer" 
                 onClick={() => setActiveTab("flight")}
             >
                 <div className="w-10 h-10 md:w-12 md:h-12 rounded-full bg-[#E1EDFE] flex items-center justify-center">
@@ -42,11 +43,5 @@ export default function Tab () {
                 Flight
             </div>
         </div>
-        <div className="mt-6">
-            {activeTab === "bus" && <div>Bus Content</div>}
-            {activeTab === "hotel" && <Blank />}
-            {activeTab === "flight" && <Blank />}
-        </div>
-        </>
     )
 }
