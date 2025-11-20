982a4b2e84d1697f9ec29c26c16a31e12351fc8e
nhuhuynh1508 <93415945+nhuhuynh1508@users.noreply.github.com>
2025-11-20 15:58:31 +0700

    Implement disabled for checkbox

--- components/reuse/dateInput.tsx
+++ components/reuse/dateInput.tsx
@@ -35,74 +35,114 @@ export default function DateInput({
   withCheckbox = false,
 }: DateInputProps) {
   const [open, setOpen] = useState(false);
-  const [date, setDate] = useState<Date | undefined>(new Date());
-  const [month, setMonth] = useState<Date | undefined>(date);
-  const [value, setValue] = useState(formatDate(date));
+  const [date, setDate] = useState<Date | undefined>();
+  const [month, setMonth] = useState<Date | undefined>(new Date());
+  const [value, setValue] = useState("");
+
+  const [checked, setChecked] = useState(false);
+  const disabled = withCheckbox && !checked;
 
   return (
     <div className="flex items-center">
       <div className="flex flex-col flex-1">
         <div className="flex flew-row gap-2">
             {withCheckbox && (
-                <Checkbox className="mt-0.5"/>
+                <Checkbox 
+                className="mt-0.5"
+                checked={checked}
+                onCheckedChange={(e) => setChecked(Boolean(e))}
+                />
             )}
             <span className="text-sm font-medium font-nunito mb-1">{label}</span>
         </div>
-        <div className="relative">
-          <Input
-            id="date"
-            value={value}
-            placeholder="June 01, 2025"
-            className="bg-background pr-10 py-6 px-3 h-10"
-            onChange={(e) => {
-              const date = new Date(e.target.value);
-              setValue(e.target.value);
-              if (isValidDate(date)) {
-                setDate(date);
-                setMonth(date);
-                onChange?.(date);
-              }
-            }}
-            onKeyDown={(e) => {
-              if (e.key === "ArrowDown") {
-                e.preventDefault();
-                setOpen(true);
-              }
-            }}
-          />
-          <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-              <Button
-                id="date-picker"
-                variant="ghost"
-                className="absolute top-1/2 right-2 h-10 w-10 -translate-y-1/2 p-2"
-              >
-                <CalendarIcon className="h-5 w-5" />
-                <span className="sr-only">Select date</span>
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent
-              className="w-auto overflow-hidden p-0"
-              align="end"
-              alignOffset={-8}
-              sideOffset={10}
-            >
-              <Calendar
-                mode="single"
-                selected={date}
-                captionLayout="dropdown"
-                month={month}
-                onMonthChange={setMonth}
-                onSelect={(date) => {
+
+        <div className="flex flex-row gap-1">
+          <div className="relative">
+            <Input
+              id="date"
+              value={value}
+              placeholder="June 01, 2025"
+              disabled={disabled}
+              className={`bg-background pr-10 py-6 px-3 h-10 w-full ${
+                disabled ? "bg-gray-100 cursor-not-allowed" : ""
+              }`}
+              onChange={(e) => {
+                const date = new Date(e.target.value);
+                setValue(e.target.value);
+                if (isValidDate(date)) {
                   setDate(date);
-                  setValue(formatDate(date));
-                  setOpen(false);
+                  setMonth(date);
                   onChange?.(date);
-                }}
-              />
-            </PopoverContent>
-          </Popover>
-        </div>
+                }
+              }}
+              onKeyDown={(e) => {
+                if (disabled) return;
+                if (e.key === "ArrowDown") {
+                  e.preventDefault();
+                  setOpen(true);
+                }
+              }}
+            />
+            
+            <Popover open={disabled ? false : open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  id="date-picker"
+                  variant="ghost"
+                  disabled={disabled}
+                  className="absolute top-1/2 right-2 -translate-y-1/2"
+                >
+                  <CalendarIcon className="h-5 w-5" />
+                  <span className="sr-only">Select date</span>
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                className="w-auto overflow-hidden p-0"
+                align="end"
+                alignOffset={-8}
+                sideOffset={10}
+              >
+                <Calendar
+                  mode="single"
+                  selected={date}
+                  captionLayout="dropdown"
+                  month={month}
+                  onMonthChange={setMonth}
+                  onSelect={(date) => {
+                    setDate(date);
+                    setValue(formatDate(date));
+                    setOpen(false);
+                    onChange?.(date);
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+          
+          <Input
+              type="time"
+              step="60"
+              disabled={disabled}
+              className={`py-6 px-3 h-10 w-[120px] ${
+                disabled ? "bg-gray-100 cursor-not-allowed" : ""
+              }`}
+              value={
+                date
+                  ? `${String(date.getHours()).padStart(2, "0")}:${String(
+                      date.getMinutes()
+                    ).padStart(2, "0")}`
+                  : ""
+              }
+              onChange={(e) => {
+                if (!date) return;
+                const [h, m] = e.target.value.split(":").map(Number);
+                const newDate = new Date(date);
+                newDate.setHours(h, m);
+                setDate(newDate);
+                onChange?.(newDate);
+              }}
+            />
+          </div>
       </div>
     </div>
   );
